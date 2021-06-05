fc290c1a69e155229ee9fd4ac6c4788281c1173c
Jack-cky <69802705+Jack-cky@users.noreply.github.com>
2021-06-05 13:52:56 +0800

    Create 14. Longest Common Prefix (Easy).py
    Day 5

---
+++ 14. Longest Common Prefix (Easy).py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ''
+        for i, value in enumerate(zip(*strs)):
+            if len(set(value)) != 1:
+                return strs[0][:i]
+        else:
+            return min(strs)
